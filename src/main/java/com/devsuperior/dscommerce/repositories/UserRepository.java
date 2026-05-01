94a39c591458f7dcfb1fcca1f0641e09771e4247
Luis-Gustavo-MC <lgmc120@gmail.com>
2026-05-01 15:25:30 -0300

    User Controller and getUserLogged

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -2,16 +2,16 @@ package com.devsuperior.dscommerce.repositories;
 
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-
     @Query(nativeQuery = true, value = """
             	SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
             	FROM tb_user
@@ -20,4 +20,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
             	WHERE tb_user.email = :email
             """)
     List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
